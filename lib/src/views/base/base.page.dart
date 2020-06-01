272f51cf078f5437670473cbd40f08cb133b5219
Kaique Munhoz <munhoz13km@gmail.com>
2020-06-01 14:23:03 -0300

    feat: Included appBar to basePage

--- lib/src/views/base/base.page.dart
+++ lib/src/views/base/base.page.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class BasePage extends StatelessWidget {
   final Widget child;
+  final AppBar appBar;
 
-  BasePage({
-    @required this.child,
-  });
+  BasePage({@required this.child, this.appBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appBar,
       body: Container(
         child: child,
       ),
