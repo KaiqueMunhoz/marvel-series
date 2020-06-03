961d907b6918caf00ecf7691494ade888a063802
Kaique Munhoz <munhoz13km@gmail.com>
2020-06-03 13:45:27 -0300

    feat: Removed unecessary properties

--- lib/src/data/models/serie.model.dart
+++ lib/src/data/models/serie.model.dart
@@ -2,42 +2,22 @@ import 'package:marvel_series/src/domain/entities/series.dart';
 import 'package:meta/meta.dart';
 
 class Response {
-  int code;
   String status;
-  String copyright;
-  String attributionText;
-  String attributionHTML;
-  String etag;
   Data data;
 
   Response({
-    this.code,
     this.status,
-    this.copyright,
-    this.attributionText,
-    this.attributionHTML,
-    this.etag,
     this.data,
   });
 
   Response.fromJson(Map<String, dynamic> json) {
-    code = json['code'];
     status = json['status'];
-    copyright = json['copyright'];
-    attributionText = json['attributionText'];
-    attributionHTML = json['attributionHTML'];
-    etag = json['etag'];
     data = json['data'] != null ? new Data.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['code'] = this.code;
     data['status'] = this.status;
-    data['copyright'] = this.copyright;
-    data['attributionText'] = this.attributionText;
-    data['attributionHTML'] = this.attributionHTML;
-    data['etag'] = this.etag;
     if (this.data != null) {
       data['data'] = this.data.toJson();
     }
@@ -106,6 +86,7 @@ class SerieModel extends Serie {
         startYear: json['startYear'],
         title: json['title']);
   }
+
   Map<String, dynamic> toJson() {
     return {
       'description': description,
