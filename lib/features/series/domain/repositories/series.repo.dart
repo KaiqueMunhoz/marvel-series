c1f2aa244c7e6e8e6655ee5b0a4274d1f090e914
Kaique Munhoz <munhoz13km@gmail.com>
2020-06-03 22:45:40 -0300

    .

--- lib/features/series/domain/repositories/series.repo.dart
+++ lib/features/series/domain/repositories/series.repo.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:marvel_series/core/error/failures.dart';
-import 'package:marvel_series/features/series/domain/entities/series.dart';
+import 'package:marvel_series/features/series/domain/entities/serie.dart';
 
 abstract class SeriesRepo {
   Future<Either<Failure, List<Serie>>> getSeries();
