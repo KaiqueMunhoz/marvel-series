b3024b5b1fa134da3ae591f4295ddc24acbc3865
Kaique Munhoz <munhoz13km@gmail.com>
2020-06-03 22:46:13 -0300

    fix: SeriesRepoImpl

--- lib/features/series/data/repositories/series.repo.impl.dart
+++ lib/features/series/data/repositories/series.repo.impl.dart
@@ -1,25 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:marvel_series/core/error/failures.dart';
 import 'package:marvel_series/core/error/exceptions.dart';
+import 'package:marvel_series/core/network/network_info.dart';
+import 'package:marvel_series/features/series/data/datasources/series_local_data_source.dart';
 import 'package:marvel_series/features/series/data/datasources/series_remote_data_source.dart';
-import 'package:marvel_series/features/series/domain/entities/series.dart';
+import 'package:marvel_series/features/series/domain/entities/serie.dart';
 import 'package:marvel_series/features/series/domain/repositories/series.repo.dart';
 import 'package:meta/meta.dart';
 
 class SeriesRepoImpl implements SeriesRepo {
   final SeriesRemoteDataSource remoteDataSource;
+  final SeriesLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
 
   SeriesRepoImpl({
     @required this.remoteDataSource,
+    @required this.localDataSource,
+    @required this.networkInfo,
   });
 
   @override
   Future<Either<Failure, List<Serie>>> getSeries() async {
-    try {
-      final remoteSeries = await remoteDataSource.seriesList();
-      return Right(remoteSeries);
-    } on ServerException {
-      return Left(ServerFailure());
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteSeries = await remoteDataSource.seriesList();
+        localDataSource.cacheSeries(remoteSeries);
+        return Right(remoteSeries);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localSerie = await localDataSource.getLastSeries();
+        return Right(localSerie);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
     }
   }
 }
