08e7ebdbfc7309817c58193814496f898b779d77
Kaique Munhoz <munhoz13km@gmail.com>
2020-06-03 22:42:26 -0300

    fix; Dependence injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,8 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:marvel_series/core/network/network_info.dart';
+import 'package:marvel_series/features/series/data/datasources/series_local_data_source.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/series/data/datasources/series_remote_data_source.dart';
@@ -21,23 +24,37 @@ void _initFeatures() {
 
   // Repository
   sl.registerLazySingleton<SeriesRepo>(
-    () => SeriesRepoImpl(remoteDataSource: sl()),
+    () => SeriesRepoImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
   );
 
   // Data sources
   sl.registerLazySingleton<SeriesRemoteDataSource>(
     () => SeriesRemoteDataSourceImpl(client: sl()),
   );
+  sl.registerLazySingleton<SeriesLocalDataSource>(
+    () => SeriesLocalDataSourceImpl(sharedPreferences: sl()),
+  );
 }
 
 Future<void> _initExternal() async {
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
-
+  sl.registerLazySingleton(() => DataConnectionChecker());
   sl.registerLazySingleton(() => http.Client());
+}
+
+_initCore() {
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
 }
 
 Future<void> init() async {
   _initFeatures();
   await _initExternal();
+  _initCore();
 }
