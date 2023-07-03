902df8f9d7e3bf5c3310edcd811db834fbe85d8c
Kaique Munhoz <munhoz13km@gmail.com>
2023-07-03 13:28:01 -0300

    fix: Upgrade flutter version to 1.20.1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:marvel_series/core/routes/routes.strings.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(Main());
 }
